62d93250078b84bd4183386b19392f1e28260df6
WorkAccVikas <workaccvikas@gmail.com>
2024-01-29 00:25:59 +0530

    c16

--- src/components/Charts.tsx
+++ src/components/Charts.tsx
@@ -185,3 +185,66 @@ export const PieChart = ({
 
   return <Pie data={pieChartData} options={pieChartOptions} />;
 };
+
+interface LineChartProps {
+  data: number[];
+  label: string;
+  backgroundColor: string;
+  borderColor: string;
+  labels?: string[];
+}
+
+export const LineChart = ({
+  data,
+  label,
+  backgroundColor,
+  borderColor,
+  labels = months,
+}: LineChartProps) => {
+  const options: ChartOptions<"line"> = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: false,
+      },
+    },
+
+    scales: {
+      y: {
+        beginAtZero: true,
+        grid: {
+          display: false,
+        },
+      },
+      x: {
+        grid: {
+          display: false,
+        },
+      },
+    },
+  };
+
+  const lineChartData: ChartData<"line", number[], string> = {
+    labels,
+    datasets: [
+      {
+        fill: true,
+        label,
+        data,
+        backgroundColor,
+        borderColor,
+      },
+    ],
+  };
+
+  return <Line options={options} data={lineChartData} />;
+};
+
+/** LEARN : For Line Chart
+ *  - LineElement, Filler from "chart.js"
+ *  - Register this 2 import in Chart JS
+ *  - fill: true in ChartData
+ */
